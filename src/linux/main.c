8f0dc214c6419aba8fad998a5f669740b2892c2c
Nicola Strappazzon C. <nstrappazzonc@gmail.com>
2024-10-24 14:29:12 +0200

    Save changes at: 1729772952

--- src/linux/main.c
+++ src/linux/main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
+#include <fcntl.h>
 
 #include "WCHBLElib/WCHBLElib.h"
 
@@ -36,23 +37,25 @@ int main() {
     return 0;
 }
 
+void suppress_stdout() {
+    fflush(stdout);
+    int fd = open("/dev/null", O_WRONLY);
+    dup2(fd, STDOUT_FILENO);
+    close(fd);
+}
+
+void restore_stdout(int saved_stdout) {
+    fflush(stdout);
+    dup2(saved_stdout, STDOUT_FILENO);
+}
+
 void scanDevices() {
     printf("==> List devices:\n");
     printf("  > Only show devices with OUI: 50:54:7B\n");
 
-    // fflush(stdout);
-    // freopen("/dev/null", "w", stdout);
-    // fclose(stdout);
-    // Guardar el stdout original
     int saved_stdout = dup(STDOUT_FILENO);
-
-    // Redirigir stdout temporalmente
     suppress_stdout();
-
     int ret = WCHBle_BLE_Scan(BLESCAN_TIMEOUT, BleAdvertisingDeviceInfo);
-    // fclose(fp);
-    //fclose(stdout);
-    // fflush(stdout);
     restore_stdout(saved_stdout);
 
     if (ret == BLE_FAILED) {
@@ -184,16 +187,4 @@ void writeToDevice() {
     
         sleep(1);
     }
-}
-
-void suppress_stdout() {
-    fflush(stdout); // Asegurarse de que no haya datos pendientes de impresión
-    int fd = open("/dev/null", O_WRONLY); // Abrir /dev/null para descartar la salida
-    dup2(fd, STDOUT_FILENO); // Redirigir stdout a /dev/null
-    close(fd); // Cerrar el descriptor de archivo
-}
-
-void restore_stdout(int saved_stdout) {
-    fflush(stdout);
-    dup2(saved_stdout, STDOUT_FILENO); // Restaurar stdout al original
 }
